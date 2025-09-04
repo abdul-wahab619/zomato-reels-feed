3246cb8ec633620f4664d257c51ab3a519b48716
abdul-wahab619 <awminhas619@gmail.com>
2025-09-04 14:01:10 +0500

    zomato: auth(frontend + cors set)

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,22 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes.js");
 const foodRoutes = require("./routes/food.routes.js");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
