ca1819fb29f633056d2f36deacc8e5f29460d09d
abdul-wahab619 <awminhas619@gmail.com>
2025-09-04 12:24:45 +0500

    zomato: food(food create, getAllFood Items)

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes.js");
+const foodRoutes = require("./routes/food.routes.js");
 
 dotenv.config();
 
 const app = express();
 
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
 
 module.exports = app;
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const authController = require("../controllers/auth.controller.js");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+// user auth APIs
 router.post("/user/register", authController.registerUser);
 router.post("/user/login", authController.loginUser);
-router.post("/user/logout", authController.logoutUser);
+router.get("/user/logout", authController.logoutUser);
+
+// food partner auth APIs
+router.post("/food-partner/register", authController.registerFoodPartner);
+router.post("/food-partner/login", authController.loginFoodPartner);
+router.get("/food-partner/logout", authController.logoutFoodPartner);
 
 module.exports = router;
